abe3a932634df98b11fb601d78652e97afae6393
Likitha s <47500211+liki2124@users.noreply.github.com>
2021-02-09 00:14:29 +0530

    update shuffle

--- Git-challenge1/src/Shuffle.java
+++ Git-challenge1/src/Shuffle.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Shuffle {
 	
-
+                      //creating main method
 		public static void main(String[] args) {
 			Scanner scan = new Scanner(System.in);
 			String word;
